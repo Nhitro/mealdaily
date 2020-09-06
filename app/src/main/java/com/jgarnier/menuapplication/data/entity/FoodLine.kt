7eb67bf515899ae6fc6485cfb6aceff46e8ae24f
jgarnier <julien53laval@gmail.com>
2020-09-06 22:57:25 +0200

    Daily Menu - move consequences

--- app/src/main/java/com/jgarnier/menuapplication/data/entity/FoodLine.kt
+++ app/src/main/java/com/jgarnier/menuapplication/data/entity/FoodLine.kt
@@ -2,7 +2,7 @@ package com.jgarnier.menuapplication.data.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.jgarnier.menuapplication.data.AmountSort
+import com.jgarnier.menuapplication.data.raw.AmountSort
 
 @Entity
 data class FoodLine(
--- app/src/main/java/com/jgarnier/menuapplication/data/entity/Meal.kt
+++ app/src/main/java/com/jgarnier/menuapplication/data/entity/Meal.kt
@@ -2,7 +2,7 @@ package com.jgarnier.menuapplication.data.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.jgarnier.menuapplication.data.MealSort
+import com.jgarnier.menuapplication.data.raw.MealSort
 import java.util.*
 
 @Entity

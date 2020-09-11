6979c2e5d9338ce3c0d8be7fea15a42c02f34ce4
jgarnier <julien53laval@gmail.com>
2020-09-11 23:19:31 +0200

    Daily Menu - changed mealDate for 3 attributes, year, month and day

--- app/src/main/java/com/jgarnier/menuapplication/data/entity/Meal.kt
+++ app/src/main/java/com/jgarnier/menuapplication/data/entity/Meal.kt
@@ -3,11 +3,12 @@ package com.jgarnier.menuapplication.data.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.jgarnier.menuapplication.data.raw.MealSort
-import java.util.*
 
 @Entity
 data class Meal(
     @PrimaryKey(autoGenerate = true) val idMeal: Int,
-    val mealDate: Date,
+    val mealYear: Int,
+    val mealMonth: Int,
+    val mealDay: Int,
     val mealSort: MealSort
 )

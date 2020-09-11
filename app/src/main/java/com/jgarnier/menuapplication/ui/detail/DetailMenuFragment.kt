287d1a2d3802a6659b24efe961e33275b7e215cf
jgarnier <julien53laval@gmail.com>
2020-09-12 00:22:51 +0200

    Daily Menu - modified DetailMenuFragment in order to show the received date into the textview

--- app/src/main/java/com/jgarnier/menuapplication/ui/detail/DetailMenuFragment.kt
+++ app/src/main/java/com/jgarnier/menuapplication/ui/detail/DetailMenuFragment.kt
@@ -5,28 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.navArgs
 import com.jgarnier.menuapplication.R
+import com.jgarnier.menuapplication.databinding.DetailMenuFragmentBinding
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class DetailMenuFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = DetailMenuFragment()
-    }
+    val args: DetailMenuFragmentArgs by navArgs()
 
-    private lateinit var viewModel: DetailMenuViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.detail_menu_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(DetailMenuViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val binding = DetailMenuFragmentBinding.bind(view)
+        val localDate = LocalDate.of(args.selectedYear, args.selectedMonth, args.selectedDay)
 
+        binding.detailMenuTest.text = localDate.format(DateTimeFormatter.ofPattern("EEEE dd MMMM YYYY"))
+    }
 }

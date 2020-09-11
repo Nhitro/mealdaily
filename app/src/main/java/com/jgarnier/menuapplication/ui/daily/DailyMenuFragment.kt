1894852e6d8b4f312786fdcb918b6960ee81c34c
jgarnier <julien53laval@gmail.com>
2020-09-12 00:23:32 +0200

    Daily Menu - DailyMenuFragment implementation

--- app/src/main/java/com/jgarnier/menuapplication/ui/daily/DailyMenuFragment.kt
+++ app/src/main/java/com/jgarnier/menuapplication/ui/daily/DailyMenuFragment.kt
@@ -1,13 +1,20 @@
 package com.jgarnier.menuapplication.ui.daily
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.OnRebindCallback
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.transition.TransitionManager
 import com.jgarnier.menuapplication.R
+import com.jgarnier.menuapplication.databinding.DailyMenuFragmentBinding
+import com.transitionseverywhere.ChangeText
 import dagger.hilt.android.AndroidEntryPoint
+import java.time.LocalDate
+
 
 /**
  * Fragment is in charge of showing the menu list
@@ -15,18 +22,45 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class DailyMenuFragment : Fragment() {
 
-    companion object {
-        fun newInstance() =
-            DailyMenuFragment()
-    }
+    private val mViewModel: DailyMenuViewModel by viewModels()
 
-    private val viewModel: DailyMenuViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mViewModel.fetchMealAccording(LocalDate.now())
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.daily_menu_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val binding = DailyMenuFragmentBinding.bind(view)
+
+        binding.apply {
+            // Set databinding values
+            lifecycleOwner = this@DailyMenuFragment
+            vm = mViewModel
+            state = mViewModel.dailyMenuState
+
+            // Initialize listeners
+            dailyMenuErrorRetry.setOnClickListener { mViewModel.retryFetchMeal() }
+            dailyMenuCalendar.setOnDateChangeListener { _, year, month, dayOfMonth -> mViewModel.fetchMealAccording(LocalDate.of(year, month + 1, dayOfMonth))}
+
+            dailyMenuEdit.setOnClickListener {
+                mViewModel.selectedDate.apply {
+                    val action = DailyMenuFragmentDirections.actionDailyMenuFragmentToDetailMenuFragment(dayOfMonth, monthValue, year)
+                    findNavController().navigate(action)
+                }
+            }
+
+            // Add callback in order to perform ChangeText() transition each time binding values change
+            addOnRebindCallback(object : OnRebindCallback<DailyMenuFragmentBinding>() {
+                override fun onPreBind(binding: DailyMenuFragmentBinding?): Boolean {
+                    TransitionManager.beginDelayedTransition(binding?.dailyMenuCardContainer as ViewGroup, ChangeText().setChangeBehavior(ChangeText.CHANGE_BEHAVIOR_OUT_IN))
+                    return super.onPreBind(binding)
+                }
+            })
+        }
 
     }
 

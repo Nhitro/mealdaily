312da4e816e31ccb5e1cce20ed444d7217aa815b
jgarnier <julien53laval@gmail.com>
2020-09-28 22:02:03 +0200

    Planning - Setup PlanningFragment & ViewModel

--- app/src/main/java/com/jgarnier/menuapplication/ui/tab_planning/PlanningFragment.kt
+++ app/src/main/java/com/jgarnier/menuapplication/ui/tab_planning/PlanningFragment.kt
@@ -6,15 +6,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.transition.TransitionManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.jgarnier.menuapplication.R
+import com.jgarnier.menuapplication.data.Result
+import com.jgarnier.menuapplication.data.entity.MealWithDishes
 import com.jgarnier.menuapplication.databinding.PlanningFragmentBinding
+import com.jgarnier.menuapplication.setLocalDate
 import com.jgarnier.menuapplication.ui.base.TransitionFragment
 import com.jgarnier.menuapplication.ui.tab_planning.PlanningViewModel.Companion.CALENDAR_VIEW
-import com.jgarnier.menuapplication.ui.tab_planning.recyclerview.WeekAdapter
+import com.jgarnier.menuapplication.ui.tab_planning.meals.MealsAdapter
+import com.jgarnier.menuapplication.ui.tab_planning.week.WeekAdapter
+import com.transitionseverywhere.ChangeText
 import dagger.hilt.android.AndroidEntryPoint
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.function.Consumer
 
 /**
@@ -27,19 +34,26 @@ class PlanningFragment : TransitionFragment(R.layout.planning_fragment) {
 
     private val mBinding: PlanningFragmentBinding by viewBinding()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    private var mSelectedDate: LocalDate? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.planning_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val adapter = WeekAdapter(LocalDate.now(), Consumer { mViewModel.userSelectedDate(it) })
+        val adapter =
+            WeekAdapter(LocalDate.now(), Consumer { mViewModel.userSelectedDate(it.localDate) })
 
         mBinding.planningDaysDates.adapter = adapter
-        mBinding.planningDaysDates.scrollToPosition(adapter.localDateNowIndex)
 
-        mBinding.planningCalendarButtonGroup.addOnButtonCheckedListener{ _, checkedId, isChecked ->
+        // Listeners
+        mBinding.planningCalendarButtonGroup.addOnButtonCheckedListener { _, checkedId, isChecked ->
             if (checkedId == R.id.planning_week_button && isChecked) {
                 mViewModel.selectWeekView()
             } else if (checkedId == R.id.planning_month_button && isChecked) {
@@ -47,19 +61,91 @@ class PlanningFragment : TransitionFragment(R.layout.planning_fragment) {
             }
         }
 
+        mBinding.planningCurrentDayLabel.setOnClickListener {
+            mBinding.planningDaysDates.smoothScrollToPosition(adapter.getSelectedPosition())
+            mBinding.planningDaysCalendar.setLocalDate(
+                mSelectedDate ?: LocalDate.now(),
+                animate = true,
+                center = false
+            )
+        }
+
+        mBinding.planningDaysCalendar.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            mViewModel.userSelectedDate(LocalDate.of(year, month + 1, dayOfMonth))
+        }
+
+        mBinding.planningFab.setOnClickListener {
+            findNavController().navigate(PlanningFragmentDirections.actionPlanningFragmentToMealDialogFragment())
+        }
+
         // View Model observation
-        mViewModel.mCurrentTypeView.observe(viewLifecycleOwner,
-            Observer {
-                with(mBinding.planningCalendarFlipper, {
-                    TransitionManager.beginDelayedTransition(mBinding.planningLayout)
-                    if (it == CALENDAR_VIEW && displayedChild != 1) {
-                        showNext()
-                    } else if (it == PlanningViewModel.WEEK_DAY_VIEW && displayedChild != 0) {
-                        showPrevious()
+        mViewModel.selectedLocalDate.observe(viewLifecycleOwner, observeSelectedLocalDate(adapter))
+        mViewModel.currentTypeView.observe(viewLifecycleOwner, observeCurrentTypeView())
+        mViewModel.mealWithDishesList.observe(viewLifecycleOwner, observeMealWithDishesResult())
+    }
+
+    private fun observeSelectedLocalDate(adapter: WeekAdapter): Observer<LocalDate> {
+        return Observer {
+            mSelectedDate = it
+
+            // Update list if needed
+            adapter.selectDate(mBinding.planningDaysDates, it)
+            // Update calendar view if needed
+            mBinding.planningDaysCalendar.setLocalDate(it, animate = false, center = true)
+
+            TransitionManager.beginDelayedTransition(
+                mBinding.planningLayout as ViewGroup,
+                ChangeText()
+                    .setChangeBehavior(ChangeText.CHANGE_BEHAVIOR_OUT_IN)
+                    .setDuration(250)
+            )
+            // Update text
+            mBinding.planningCurrentDayLabel.text = getCapitalizeDate(it)
+        }
+    }
+
+    private fun observeCurrentTypeView(): Observer<Int> {
+        return Observer {
+            with(mBinding.planningCalendarFlipper, {
+                TransitionManager.beginDelayedTransition(mBinding.planningLayout)
+                // Managing transition calendar <- week days list
+                if (it == CALENDAR_VIEW && displayedChild != 1) {
+                    showNext()
+                } else if (it == PlanningViewModel.WEEK_DAY_VIEW && displayedChild != 0) {
+                    showPrevious()
+                }
+            })
+        }
+    }
+
+    private fun observeMealWithDishesResult(): Observer<Result<List<MealWithDishes>>> {
+        return Observer {
+            it?.apply {
+                if (it is Result.Loading) {
+                    mBinding.dayMealsLoader.visibility = View.VISIBLE
+                    mBinding.dayMeals.visibility = View.INVISIBLE
+                } else {
+                    mBinding.dayMealsLoader.visibility = View.GONE
+                    mBinding.dayMeals.visibility = View.VISIBLE
+
+                    if (it.data != null && it.data.isNotEmpty()) {
+                        mBinding.dayMealsInformation.visibility = View.GONE
+                        mBinding.dayMeals.adapter = MealsAdapter(it.data)
+                    } else {
+                        mBinding.dayMealsInformation.visibility = View.VISIBLE
                     }
-                })
+                }
             }
-        )
+        }
+    }
+
+    /**
+     * @return localDate into "EEEE dd MMMM" format and capitalize each word (exp : Tuesday 14 September)
+     */
+    private fun getCapitalizeDate(localDate: LocalDate): String {
+        return localDate.format(DateTimeFormatter.ofPattern("EEEE dd MMM"))
+            .split(" ")
+            .joinToString(" ") { it.capitalize() }
     }
 
 }
--- app/src/main/java/com/jgarnier/menuapplication/ui/tab_planning/PlanningViewModel.kt
+++ app/src/main/java/com/jgarnier/menuapplication/ui/tab_planning/PlanningViewModel.kt
@@ -5,41 +5,76 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import com.jgarnier.menuapplication.ui.tab_planning.recyclerview.WeekDate
+import androidx.lifecycle.viewModelScope
+import com.jgarnier.menuapplication.data.Result
+import com.jgarnier.menuapplication.data.Result.Loading
+import com.jgarnier.menuapplication.data.entity.MealWithDishes
+import com.jgarnier.menuapplication.data.repository.MealRepository
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import java.time.LocalDate
 
 /**
  * Represents the state of [PlanningFragment]
  */
 class PlanningViewModel @ViewModelInject constructor(
-    @Assisted private val savedStateHandle: SavedStateHandle
+    @Assisted private val savedStateHandle: SavedStateHandle,
+    private val mealRepository: MealRepository
 ) : ViewModel() {
 
     companion object {
-        private const val TYPE_OF_VIEW = "select_date_key_bundle"
+        private const val LAST_DATE = "last_date_key_bundle"
+        private const val TYPE_OF_VIEW = "last_type_of_view_key_bundle"
+        private const val MEAL_AND_DISHES = "last_meals_and_dishes_key_bundle"
         const val CALENDAR_VIEW = 1
         const val WEEK_DAY_VIEW = 2
     }
 
-    val mCurrentTypeView: MutableLiveData<Int>
+    val selectedLocalDate: MutableLiveData<LocalDate>
+    val currentTypeView: MutableLiveData<Int>
+    val mealWithDishesList: MutableLiveData<Result<List<MealWithDishes>>>
 
     init {
-        mCurrentTypeView = savedStateHandle.get<MutableLiveData<Int>>(TYPE_OF_VIEW) ?: MutableLiveData(WEEK_DAY_VIEW)
+        selectedLocalDate = savedStateHandle.get<MutableLiveData<LocalDate>>(LAST_DATE)
+            ?: MutableLiveData(LocalDate.now())
+        currentTypeView =
+            savedStateHandle.get<MutableLiveData<Int>>(TYPE_OF_VIEW) ?: MutableLiveData(
+                WEEK_DAY_VIEW
+            )
+
+        val savedMealWithDishes =
+            savedStateHandle.get<MutableLiveData<Result<List<MealWithDishes>>>>(MEAL_AND_DISHES)
+
+        if (savedMealWithDishes == null) {
+            mealWithDishesList = MutableLiveData(Loading())
+            userSelectedDate(selectedLocalDate.value!!)
+        } else {
+            mealWithDishesList = savedMealWithDishes
+        }
     }
 
     override fun onCleared() {
-        savedStateHandle.set(TYPE_OF_VIEW, mCurrentTypeView)
+        savedStateHandle.set(TYPE_OF_VIEW, currentTypeView)
+        savedStateHandle.set(MEAL_AND_DISHES, mealWithDishesList)
+        savedStateHandle.set(LAST_DATE, selectedLocalDate)
     }
 
-    fun userSelectedDate(weekDate: WeekDate) {
-        // TODO: Implement this method
+    fun userSelectedDate(localDate: LocalDate) {
+        selectedLocalDate.postValue(localDate)
+
+        viewModelScope.launch {
+            mealRepository
+                .getDailyMealAccording(localDate)
+                .collect { mealWithDishesList.postValue(Result.Success(it)) }
+        }
     }
 
     fun selectWeekView() = postTypeViewIfDifferent(WEEK_DAY_VIEW)
     fun selectMonthView() = postTypeViewIfDifferent(CALENDAR_VIEW)
 
     private fun postTypeViewIfDifferent(typeView: Int) {
-        if (mCurrentTypeView.value != null && mCurrentTypeView.value != typeView) {
-            mCurrentTypeView.postValue(typeView)
+        if (currentTypeView.value != null && currentTypeView.value != typeView) {
+            currentTypeView.postValue(typeView)
         }
     }
 

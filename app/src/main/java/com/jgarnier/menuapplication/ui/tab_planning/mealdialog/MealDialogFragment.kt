b9c6b6c4e552f9e7005a369f660bae59b768edad
jgarnier <julien53laval@gmail.com>
2020-09-28 22:47:48 +0200

    Planning - Set default value of radio group to BREAK_FAST, added ViewModel and arguments

--- app/src/main/java/com/jgarnier/menuapplication/ui/tab_planning/mealdialog/MealDialogFragment.kt
+++ app/src/main/java/com/jgarnier/menuapplication/ui/tab_planning/mealdialog/MealDialogFragment.kt
@@ -5,13 +5,20 @@ import android.os.Bundle
 import android.widget.LinearLayout
 import android.widget.RadioButton
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.jgarnier.menuapplication.R
 import com.jgarnier.menuapplication.data.raw.MealSort
 import com.jgarnier.menuapplication.databinding.FragmentMealDialogBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class MealDialogFragment : DialogFragment() {
+
+    private val mArgs: MealDialogFragmentArgs by navArgs()
+
+    private val mViewModel: MealDialogViewModel by viewModels()
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let { activity ->
@@ -22,22 +29,28 @@ class MealDialogFragment : DialogFragment() {
             val databinding = FragmentMealDialogBinding.bind(rootView)
 
             MealSort.values().map {
-                databinding.dialogRadioGroup.addView(createRadioButton(mealSort = it))
+                databinding.mealDialogRadioGroup.addView(createRadioButton(mealSort = it))
             }
+
+            databinding.mealDialogRadioGroup.check(MealSort.BREAK_FAST.ordinal)
 
             builder.setTitle(R.string.meal_dialog_title)
             builder.setView(rootView)
-            builder.setPositiveButton(R.string.dialog_validate_label, { dialog, which -> })
-            builder.setNegativeButton(R.string.dialog_cancel_label, { dialog, which -> })
-
-            val dialog = builder.create()
-            dialog
+            builder.setPositiveButton(R.string.dialog_validate_label) { _, _ ->
+                mViewModel.insert(
+                    mArgs.selectedDate
+                )
+            }
+            builder.setNegativeButton(R.string.dialog_cancel_label) { _, _ -> dismiss() }
+            builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
     }
 
     private fun createRadioButton(mealSort: MealSort): RadioButton {
         val mealsArray = resources.getStringArray(R.array.meals_array)
         val radioButton = RadioButton(requireContext())
+
+        radioButton.id = mealSort.ordinal
 
         radioButton.layoutParams = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.MATCH_PARENT,

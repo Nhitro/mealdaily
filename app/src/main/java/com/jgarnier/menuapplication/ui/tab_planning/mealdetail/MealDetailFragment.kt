c79bb72007649d4ccc0efa327267eef7a4872b32
Julien <julien53laval@gmail.com>
2020-10-25 15:30:26 +0100

    MealDetail - Implemented both MealDetailFragment and MealDetailViewModel

--- app/src/main/java/com/jgarnier/menuapplication/ui/tab_planning/mealdetail/MealDetailFragment.kt
+++ app/src/main/java/com/jgarnier/menuapplication/ui/tab_planning/mealdetail/MealDetailFragment.kt
@@ -3,20 +3,63 @@ package com.jgarnier.menuapplication.ui.tab_planning.mealdetail
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.jgarnier.menuapplication.R
-import com.jgarnier.menuapplication.databinding.FragmentDetailMenuBinding
+import com.jgarnier.menuapplication.data.Result
+import com.jgarnier.menuapplication.databinding.FragmentMealDetailBinding
 import com.jgarnier.menuapplication.ui.base.TransitionFragment
+import com.jgarnier.menuapplication.ui.tab_planning.dishes.DishesAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 
+@FlowPreview
+@ExperimentalCoroutinesApi
 @AndroidEntryPoint
 class MealDetailFragment : TransitionFragment(R.layout.fragment_meal_detail) {
 
     private val mViewModel: MealDetailViewModel by viewModels()
 
-    private val mBinding: FragmentDetailMenuBinding by viewBinding()
+    private val mBinding: FragmentMealDetailBinding by viewBinding()
+
+    private val mArgs: MealDetailFragmentArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mViewModel.fetchMealAndDishes(mArgs.day, mArgs.month, mArgs.year, mArgs.mealSort)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val adapter = DishesAdapter()
+
+        mBinding.menuDetailTitle.setNavigationOnClickListener { findNavController().popBackStack() }
+        mBinding.menuDetailList.adapter = adapter
+
+        mViewModel.fetchedData.observe(viewLifecycleOwner, Observer {
+            if (it is Result.Loading) {
+                mBinding.menuDetailLoader.visibility = View.VISIBLE
+                mBinding.menuDetailList.visibility = View.INVISIBLE
+                mBinding.menuDetailEmpty.visibility = View.GONE
+            } else {
+                val dishes = it.data ?: ArrayList()
+                mBinding.menuDetailLoader.visibility = View.GONE
+                mBinding.menuDetailList.visibility = View.VISIBLE
+
+                if (dishes.isEmpty()) {
+                    mBinding.menuDetailEmpty.visibility = View.VISIBLE
+                } else {
+                    mBinding.menuDetailEmpty.visibility = View.GONE
+                }
+
+                adapter.submitList(dishes)
+            }
+        })
+
+        mViewModel.mealTitle.observe(viewLifecycleOwner, Observer { mBinding.menuDetailTitle.title = it })
     }
 }
--- app/src/main/java/com/jgarnier/menuapplication/ui/tab_planning/mealdetail/MealDetailViewModel.kt
+++ app/src/main/java/com/jgarnier/menuapplication/ui/tab_planning/mealdetail/MealDetailViewModel.kt
@@ -1,7 +1,81 @@
 package com.jgarnier.menuapplication.ui.tab_planning.mealdetail
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.hilt.Assisted
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
+import com.jgarnier.menuapplication.R
+import com.jgarnier.menuapplication.data.entity.Dish
+import com.jgarnier.menuapplication.data.entity.MealWithDishes
+import com.jgarnier.menuapplication.data.raw.MealSort
+import com.jgarnier.menuapplication.data.repository.DishRepository
+import com.jgarnier.menuapplication.data.repository.MealRepository
+import com.jgarnier.menuapplication.ui.base.AbstractListViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
-class MealDetailViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@ExperimentalCoroutinesApi
+@FlowPreview
+class MealDetailViewModel @ViewModelInject constructor(
+        application: Application,
+        @Assisted private val savedStateHandle: SavedStateHandle,
+        private val mMealRepository: MealRepository,
+        private val mDishRepository: DishRepository
+) : AbstractListViewModel<Int, Dish>(application) {
+
+    private val mealTitleLiveData: MutableLiveData<String> = MutableLiveData()
+
+    private var mealWithDishes: MealWithDishes? = null
+
+    val mealTitle: LiveData<String>
+        get() = mealTitleLiveData
+
+    override suspend fun fetchData(filterObject: Int): Flow<List<Dish>> {
+        return mDishRepository.getDishesAssociatedToMeal(filterObject)
+    }
+
+    fun fetchMealAndDishes(day: Int, month: Int, year: Int, mealSort: String) {
+        val mealSortConverted = MealSort.valueOf(mealSort)
+        computeTitle(day, month, year, mealSortConverted)
+        viewModelScope.launch {
+            mMealRepository.findMealWithDishes(day, month, year, mealSortConverted).apply {
+                mealWithDishes = this
+                mFilterObjectChannel.offer(this.meal.idMeal)
+            }
+        }
+    }
+
+    private fun computeTitle(day: Int, month: Int, year: Int, mealSortConverted: MealSort) {
+        val context = getApplication<Application>()
+        val date = getCapitalizeDate(LocalDate.of(year, month, day))
+        val idMessage =
+                when (mealSortConverted) {
+                    MealSort.BREAK_FAST -> R.string.meal_break_fast
+                    MealSort.LUNCH -> R.string.meal_lunch
+                    MealSort.DINNER -> R.string.meal_dinner
+                    MealSort.SNACK -> R.string.meal_snack
+                }
+
+        mealTitleLiveData.postValue(
+                context.getString(
+                        R.string.detail_meal_title,
+                        context.getString(idMessage),
+                        date
+                )
+        )
+    }
+
+    private fun getCapitalizeDate(localDate: LocalDate): String {
+        return localDate.format(DateTimeFormatter.ofPattern("EE dd MMM"))
+                .split(" ")
+                .joinToString(" ") { it.capitalize() }
+    }
+
 }

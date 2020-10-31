8da9665fa22cb8669ed05c8f5866c9952e53e356
Julien <julien53laval@gmail.com>
2020-10-31 18:39:59 +0100

    DishSearch - Created DishesSearchFragment and related files

---
+++ app/src/main/java/com/jgarnier/menuapplication/ui/tab_planning/dishessearch/DishesSearchFragment.kt
@@ -0,0 +1,8 @@
+package com.jgarnier.menuapplication.ui.tab_planning.dishessearch
+
+import com.jgarnier.menuapplication.R
+import com.jgarnier.menuapplication.ui.base.TransitionFragment
+
+class DishesSearchFragment : TransitionFragment(R.layout.fragment_dishes_search) {
+
+}
---
+++ app/src/main/java/com/jgarnier/menuapplication/ui/tab_planning/dishessearch/DishesSearchViewModel.kt
@@ -0,0 +1,7 @@
+package com.jgarnier.menuapplication.ui.tab_planning.dishessearch
+
+import androidx.lifecycle.ViewModel
+
+class DishesSearchViewModel : ViewModel() {
+
+}

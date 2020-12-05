e8270b96eff2f0733a742cf080da0d57d5d44f23
Julien <julien53laval@gmail.com>
2020-12-05 12:27:17 +0100

    DishSearch - added consumer for view holder

--- app/src/main/java/com/jgarnier/menuapplication/ui/tab_planning/dishessearch/list/DishWithLineAdapter.kt
+++ app/src/main/java/com/jgarnier/menuapplication/ui/tab_planning/dishessearch/list/DishWithLineAdapter.kt
@@ -5,11 +5,16 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import com.jgarnier.menuapplication.R
 import com.jgarnier.menuapplication.data.entity.DishWithLines
+import java.util.function.Consumer
 
-class DishWithLineAdapter : ListAdapter<DishWithLines, DishWithLineHolder>(DishWithLineDiffUtils()) {
+class DishWithLineAdapter(private val mOnClickConsumer: Consumer<DishWithLines>) :
+    ListAdapter<DishWithLines, DishWithLineHolder>(DishWithLineDiffUtils()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DishWithLineHolder {
-        return DishWithLineHolder(LayoutInflater.from(parent.context).inflate(R.layout.holder_dish, parent, false))
+        return DishWithLineHolder(
+            mOnClickConsumer,
+            LayoutInflater.from(parent.context).inflate(R.layout.holder_dish, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: DishWithLineHolder, position: Int) {
--- app/src/main/java/com/jgarnier/menuapplication/ui/tab_planning/dishessearch/list/DishWithLineHolder.kt
+++ app/src/main/java/com/jgarnier/menuapplication/ui/tab_planning/dishessearch/list/DishWithLineHolder.kt
@@ -4,13 +4,18 @@ import android.view.View
 import com.jgarnier.menuapplication.data.entity.DishWithLines
 import com.jgarnier.menuapplication.databinding.HolderDishBinding
 import com.jgarnier.menuapplication.ui.base.AbstractViewHolder
+import java.util.function.Consumer
 
-class DishWithLineHolder(itemView: View) : AbstractViewHolder<DishWithLines>(itemView) {
+class DishWithLineHolder(private val mOnClickConsumer: Consumer<DishWithLines>, itemView: View) :
+    AbstractViewHolder<DishWithLines>(itemView) {
 
     private val mBinding = HolderDishBinding.bind(itemView)
 
     override fun update(data: DishWithLines) {
         mBinding.dishName.text = data.dish.dishName
+        mBinding.root.setOnClickListener {
+            mOnClickConsumer.accept(data)
+        }
     }
 
 }

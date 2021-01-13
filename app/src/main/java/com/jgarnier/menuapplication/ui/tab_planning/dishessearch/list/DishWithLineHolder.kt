33c78ac3c9a29a6ec36f8762574355ccb106400b
Julien <julien53laval@gmail.com>
2021-01-13 21:47:59 +0100

    DishSearch - Changed DishWithLineHolder UI

--- app/src/main/java/com/jgarnier/menuapplication/ui/tab_planning/dishessearch/list/DishWithLineHolder.kt
+++ app/src/main/java/com/jgarnier/menuapplication/ui/tab_planning/dishessearch/list/DishWithLineHolder.kt
@@ -1,6 +1,7 @@
 package com.jgarnier.menuapplication.ui.tab_planning.dishessearch.list
 
 import android.view.View
+import com.jgarnier.menuapplication.R
 import com.jgarnier.menuapplication.data.entity.DishWithLines
 import com.jgarnier.menuapplication.databinding.HolderDishBinding
 import com.jgarnier.menuapplication.ui.base.AbstractViewHolder
@@ -15,6 +16,16 @@ class DishWithLineHolder(private val mOnClickConsumer: Consumer<DishWithLines>, 
         mBinding.dishName.text = data.dish.dishName
         mBinding.root.setOnClickListener {
             mOnClickConsumer.accept(data)
+        }
+
+        val dishWithLinesList = data.dishLines
+
+        if (dishWithLinesList.isNullOrEmpty()) {
+            mBinding.dishIngredients.visibility = View.INVISIBLE
+        } else {
+            val ingredientText = dishWithLinesList.joinToString(", ") { dishLine -> dishLine.foodName }
+            mBinding.dishIngredients.visibility = View.VISIBLE
+            mBinding.dishIngredients.text = mBinding.root.context.getString(R.string.dish_cell_ingredient_label, ingredientText.substring(0, ingredientText.length))
         }
     }
 
